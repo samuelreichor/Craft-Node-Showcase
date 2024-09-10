e95962c553d442b241873944e4a5abafd628a582
samuelreichor <samuelreichor@gmail.com>
2024-09-10 20:43:21 +0200

    Add boilerplate for noder plugin

---
+++ backend-craftcms/plugins/craft-noder/CHANGELOG.md
@@ -0,0 +1,4 @@
+# Release Notes for craft-noder
+
+## 1.0.0
+- Initial release
---
+++ backend-craftcms/plugins/craft-noder/src/Noder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace samuelreichoer\craftnoder;
+
+use Craft;
+use craft\base\Plugin;
+
+/**
+ * craft-noder plugin
+ *
+ * @method static Noder getInstance()
+ * @author Samuel Reichör <samuelreichor@gmail.com>
+ * @copyright Samuel Reichör
+ * @license MIT
+ */
+class Noder extends Plugin
+{
+    public string $schemaVersion = '1.0.0';
+
+    public static function config(): array
+    {
+        return [
+            'components' => [
+                // Define component configs here...
+            ],
+        ];
+    }
+
+    public function init(): void
+    {
+        parent::init();
+
+        $this->attachEventHandlers();
+
+        // Any code that creates an element query or loads Twig should be deferred until
+        // after Craft is fully initialized, to avoid conflicts with other plugins/modules
+        Craft::$app->onInit(function() {
+            // ...
+        });
+    }
+
+    private function attachEventHandlers(): void
+    {
+        // Register event handlers here ...
+        // (see https://craftcms.com/docs/5.x/extend/events.html to get started)
+    }
+}
